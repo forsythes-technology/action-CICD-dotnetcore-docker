ee95e3674f7c524a863947a5d52a47af5a2e6ba6
Andrew McMahon <53245014+FTAmcmahon@users.noreply.github.com>
2021-03-17 17:42:43 +1100

    build from last

--- dist/index.js
+++ dist/index.js
@@ -4073,9 +4073,9 @@ function main() {
                 // generate a package for each project and push to Octopus
                 if (dbupProject) {
                     core.info(`Deploying DbUp project: ${dbupProject}`);
-                    yield exec_1.exec(`$HOME/.dotnet/tools/dotnet-octo pack --id=${dbupProject} --outFolder=${dbupProject}/artifacts --basePath=${dbupProject}/output --version=${version}`);
+                    yield exec_1.exec(`/$HOME/.dotnet/tools/dotnet-octo pack --id=${dbupProject} --outFolder=${dbupProject}/artifacts --basePath=${dbupProject}/output --version=${version}`);
                     core.info(`Push ${dbupProject} to Octopus...`);
-                    yield exec_1.exec(`$HOME/.dotnet/tools/dotnet-octo push --package=${dbupProject}/artifacts/${dbupProject}.${version}.nupkg --server=${octopusUrl} --apiKey=${octopusApiKey}`);
+                    yield exec_1.exec(`/$HOME/.dotnet/tools/dotnet-octo push --package=${dbupProject}/artifacts/${dbupProject}.${version}.nupkg --server=${octopusUrl} --apiKey=${octopusApiKey}`);
                 }
                 core.info(dockerProject);
                 core.info(`Building Docker Image: ${repoName}`);
@@ -4089,7 +4089,7 @@ function main() {
                 yield exec_1.exec(`docker push ${imageTag}`);
                 core.info(`Push complete`);
                 core.info("Creating Release...");
-                yield exec_1.exec(`$HOME/.dotnet/tools/dotnet-octo create-release --project=${repoName} --version=${version} --server=${octopusUrl} --apiKey=${octopusApiKey}`);
+                yield exec_1.exec(`/$HOME/.dotnet/tools/dotnet-octo create-release --project=${repoName} --version=${version} --server=${octopusUrl} --apiKey=${octopusApiKey}`);
                 if (msTeamsWebhook) {
                     sendNotification_1.sendTeamsNotification(repoName, `✔ Version ${version} Deployed to Octopus`, msTeamsWebhook);
                 }
